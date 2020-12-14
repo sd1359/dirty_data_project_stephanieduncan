cdc94d170920f9436c86588c0e099c51f7dfb3c0
sd1359 <stephanieduncan@outlook.com>
2020-12-14 11:39:35 +0000

    Joining two sheets.

--- task3/data_cleaning_scripts/data_cleaning_script.R
+++ task3/data_cleaning_scripts/data_cleaning_script.R
@@ -16,10 +16,18 @@ excel_sheets("raw_data/seabirds.xls")
 #Extracting the "Bird data by record ID" sheet from .xls file and cleaning the names
 bird_data <- read_excel("raw_data/seabirds.xls", sheet = "Bird data by record ID") %>% clean_names()
 
+#Getting the variable names and types for bird_data
+names(bird_data)
+glimpse(bird_data)
+
 #Extracting the "Ship data by record ID" sheet from .xls file and cleaning the names
 ship_data <- read_excel("raw_data/seabirds.xls", sheet = "Ship data by record ID") %>% clean_names()
 
-#Joining the two sheets "Ship data by record ID" and "Bird data by record ID" together
+#Getting the variable names and types for ship_data
+names(ship_data)
+glimpse(ship_data)
+
+#Joining the two sheets "Ship data by record ID" and "Bird data by record ID" together so all rows of both sheets are kept
 seabirds_data <- full_join(ship_data, bird_data, by = "record_id")
 
 
