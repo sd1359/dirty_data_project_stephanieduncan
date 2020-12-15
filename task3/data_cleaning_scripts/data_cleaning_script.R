c5ea2f79f1c228a63cdf08b950ca3713666fb501
sd1359 <stephanieduncan@outlook.com>
2020-12-15 11:10:30 +0000

    Adding updates after cleaning further.

--- task3/data_cleaning_scripts/data_cleaning_script.R
+++ task3/data_cleaning_scripts/data_cleaning_script.R
@@ -7,8 +7,6 @@ library(dplyr)
 library(tidyr)
 library(assertr)
 
-#Testing where the top level of the project directory is
-here::here()
 
 #List the sheet names contained in the .xls file
 excel_sheets("raw_data/seabirds.xls")
@@ -16,7 +14,7 @@ excel_sheets("raw_data/seabirds.xls")
 #Reading in the raw seabird data, extracting the "Bird data by record ID" sheet and cleaning the names
 bird_data <- read_excel("raw_data/seabirds.xls", sheet = "Bird data by record ID") %>% clean_names()
 
-#Getting the variable names and types for bird_data
+#Looking at the variable names and types for bird_data
 names(bird_data)
 glimpse(bird_data)
 dim(bird_data)
@@ -24,7 +22,7 @@ dim(bird_data)
 #Extracting the "Ship data by record ID" sheet from .xls file and cleaning the names
 ship_data <- read_excel("raw_data/seabirds.xls", sheet = "Ship data by record ID") %>% clean_names()
 
-#Getting the variable names and types for ship_data
+#Looking at the variable names and types for ship_data
 names(ship_data)
 glimpse(ship_data)
 dim(ship_data)
@@ -33,13 +31,16 @@ dim(ship_data)
 seabirds_data <- full_join(ship_data, bird_data, by = "record_id") %>% 
   #Renaming column names
   rename(bird_common_name = species_common_name_taxon_age_sex_plumage_phase,
-         bird_scientific_name = species_scientific_name_taxon_age_sex_plumage_phase)
+         bird_scientific_name = species_scientific_name_taxon_age_sex_plumage_phase,
+         number_of_birds = count)
+
+#An overall view shows that the data is about sightings of bird species by ships between 1969 and 1990.
 
 names(seabirds_data)
 
 seabirds_data <- seabirds_data %>% 
-#Keeping the following columns: bird's common name, scientific name, species abbreviation, latitude, longitude, record id.
-select(record_id, bird_common_name, bird_scientific_name, species_abbreviation, lat, long, count) 
+#Keeping the following columns: bird's common name, scientific name, species abbreviation, latitude, record id.
+select(record_id, bird_common_name, bird_scientific_name, species_abbreviation, lat, long, number_of_birds, date) 
 
 #Finding any missing values - 1st Stage
 seabirds_data %>% 
@@ -49,7 +50,7 @@ seabirds_data %>%
 missing_values <- seabirds_data %>% 
   filter(is.na(bird_scientific_name))
 
-#Delete records where bird_common_name states "[NO BIRDS RECORDED]
+#Delete records where bird_common_name states "[NO BIRDS RECORDED]"
 seabirds_data <- 
 filter(seabirds_data, bird_common_name != "[NO BIRDS RECORDED]")
 
@@ -76,9 +77,9 @@ seabirds_data %>%
 #The above check showed there are missing values in count column. 
 #Taking a closer look.
 missing_values <- seabirds_data %>% 
-  filter(is.na(count))
-#There are 2008 rows of missing count values - drop these.
-seabirds_data <- seabirds %>% 
+  filter(is.na(number_of_birds))
+#There are many rows of missing count values - drop these.
+seabirds_data <- seabirds_data %>% 
   drop_na()
 
 #Writing the cleaned data to a csv file.
